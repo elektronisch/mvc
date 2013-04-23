b71b990d61ab85a7648b65c93cbc5e15f5a2e302
Jason Mitchell <jason.mitchell.w@gmail.com>
2013-04-23 00:10:39 -0400

    Guice IOC

--- app/Global.scala
+++ app/Global.scala
@@ -2,32 +2,31 @@ import com.google.inject.Guice
 import com.tzavellas.sse.guice.ScalaModule
 
 import play.api._
-
+import play.api.Play.current
 
 // todo: make DevModule inherit from ProdModule and override/manipulate a set of binds
-//class ProdModule extends ScalaModule {
-//  def configure() {
-//bind[Translator].to[FrenchTranslatorImpl]
-//  }
-//}
+class ProdModule extends ScalaModule {
+  def configure() {
+    //bind[Translator].to[FrenchTranslatorImpl]
+  }
+}
 
-//class DevModule extends ScalaModule {
-//  def configure() {
-//bind[Translator].to[FakeTranslator]
-//  }
-//}
+class DevModule extends ScalaModule {
+  def configure() {
+    //bind[Translator].to[FakeTranslator]
+  }
+}
 
 object Global extends GlobalSettings {
-//  private lazy val injector = {
-//    Play.isProd match {
-//      case true => Guice.createInjector(new ProdModule)
-//      case false => Guice.createInjector(new DevModule)
-//    }
-//  }
+  private lazy val injector = Play.isProd match {
+    case true => Guice.createInjector(new ProdModule)
+    case false => Guice.createInjector(new DevModule)
+  }
 
-//  override def getControllerInstance[A](clazz: Class[A]) = {
-//    injector.getInstance(clazz)
-//  }
+  override def getControllerInstance[A](clazz: Class[A]) = {
+    injector.getInstance(clazz)
+  }
+
   override def onStart(app: Application) {
     super.onStart(app)
     Logger.info("Application has started")
